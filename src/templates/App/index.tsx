06c6f5c6ece0d2614f76f41ee09afe3e3c2d77c3
Gabriel Campos <gabrielsouza.ccampos@gmail.com>
2022-08-27 15:23:58 -0300

    feat: add template

---
+++ src/templates/App/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './styles'
+
+interface IApp  {}
+
+const App: React.FC<IApp> = ({}) => {
+  return (
+    <S.Container>App</S.Container>
+  )
+}
+
+export default App
